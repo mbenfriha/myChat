193e31a82b1cfd4c12b624e960d6228e630636e9
mohamed.benfriha@epitech.eu <mohamed.benfriha@epitech.eu@epitech.eu>
2019-05-16 09:34:51 +0200

    update img crop

--- my-chat-app/src/app/app.module.ts
+++ my-chat-app/src/app/app.module.ts
@@ -1,18 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UploadAvatarComponent } from './upload-avatar/upload-avatar.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LyThemeModule, LY_THEME } from '@alyle/ui';
+import { MinimaLight } from '@alyle/ui/themes/minima';
+
+
+import { LyButtonModule } from '@alyle/ui/button';
+import { LyToolbarModule } from '@alyle/ui/toolbar';
+import { LyResizingCroppingImageModule } from '@alyle/ui/resizing-cropping-images';
+import { LyIconModule } from '@alyle/ui/icon';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadAvatarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    LyThemeModule.setTheme('minima-light'),
+    BrowserAnimationsModule,
+    LyButtonModule,
+    LyToolbarModule,
+    LyResizingCroppingImageModule,
+    CommonModule,
+    FormsModule,
+    LyIconModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+
+  providers: [{ provide: LY_THEME, useClass: MinimaLight, multi: true }],
+  bootstrap: [AppComponent],
+  exports: [UploadAvatarComponent],
 })
 export class AppModule { }
